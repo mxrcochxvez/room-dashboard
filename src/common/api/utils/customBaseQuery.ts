039afddb725ebe6d71f19141fe0b7e93ca0dac0c
Marco Chavez <43889446+mxrcochxvez@users.noreply.github.com>
2022-11-15 16:35:43 -0800

    feat(api key): removes key for security purposes

--- src/common/api/utils/customBaseQuery.ts
+++ src/common/api/utils/customBaseQuery.ts
@@ -1,4 +1,5 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+import { apiKey } from "../../../env/apiKey";
 
 /**
  * The custom base query prepares the headers of each request in order to communicate with the azure functions
@@ -6,10 +7,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 const customBaseQuery = fetchBaseQuery({
   baseUrl: "https://destifyfunc-api-dev.azurewebsites.net/api/rooms",
   prepareHeaders: (headers) => {
-    headers.set(
-      "x-functions-key",
-      "trsmthTaK7p/CS6CSQamg0zB9xxmd9w5COrtM9vS1azadc4sksMYPA=="
-    );
+    headers.set("x-functions-key", apiKey);
     return headers;
   },
 });
---
+++ src/env/apiKey.ts
@@ -0,0 +1,3 @@
+// The application will not work properly without providing this API key.
+
+export const apiKey = "";
